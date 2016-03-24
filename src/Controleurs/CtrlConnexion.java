85ca4f156b18a875ebf3fc2e6ef709c75e9f1bfe
btssio <btssio@btssio-VirtualBox>
2016-03-24 15:58:54 +0100

    connexion sammy

---
+++ src/Controleurs/CtrlConnexion.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controleurs;
+
+import Controleurs.CtrlMenu;
+import Vues.VueConnexion;
+import Vues.VueMenu;
+//import Vues.Menu;
+import Vues.VueVisiteurs;
+import java.awt.Menu;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import modele_DAO.Dao;
+import modele_Metier.MetierVisiteur;
+
+/**
+ *
+ * @author btssio
+ */
+public class CtrlConnexion implements ActionListener {
+
+    private final VueConnexion vue;
+    private MetierVisiteur myVisiteur;
+
+    public CtrlConnexion(VueConnexion vue) {
+        this.vue = vue;
+        vue.getjButtonConnection().addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        if (source == vue.getjButtonConnection()) {
+
+            try {
+                String login = vue.getjTextFieldLogin().getText();
+                String pass =  vue.getjTextFieldMDP().getText();
+                myVisiteur = Dao.getConnection(login, pass);                
+                if (myVisiteur != null) {
+                    VueMenu vue = new VueMenu();
+                    CtrlMenu controllers = new CtrlMenu(vue);
+                    vue.setVisible(true);
+                } else {
+                    System.out.println("erreur");
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(CtrlMenu.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(CtrlMenu.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+        
+    }
+    
+    public MetierVisiteur getVisiteur(MetierVisiteur unVisiteur) {
+        unVisiteur = myVisiteur;
+        return unVisiteur;
+        
+    }
+
+}
